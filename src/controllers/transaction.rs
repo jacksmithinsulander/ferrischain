c86ec4476c5ee7b6696688dfc45381b1cad15a74
jacksmithinsulander <666.jack.smith@protonmail.com>
2025-03-25 00:02:56 +0100

    fix

--- src/controllers/transaction.rs
+++ src/controllers/transaction.rs
@@ -1,5 +1,9 @@
 //! # This module responsible for everything regarding transactions
 
+use k256::schnorr::Signature;
+use crate::controllers::wallet::Wallet;
+use std::fmt;
+
 /// Transaction struct, contains all relevant info that together creates the transaction
 /// 
 /// # Fields
@@ -10,18 +14,28 @@
 /// - 'gas': Amount of gas sent to the transaction as reward for the miner including it to the chain
 /// - 'nonce': Payees nonce, linked to this transaction, typically an ascending number
 /// - 'timestamp': Timestamp where the transaction was sent to the mempool
-/// - 'hash': All information of this transaction, hashed together
 /// - 'message': An optional message field that the payee can use to write something fun to the recipient
 #[derive(Clone)]
-pub struct Transaction {
+pub struct TxInfo {
     pub payee: String,
     pub recipient: String,
     pub value: u64,
     pub gas: u64,
     pub nonce: u64,
     pub timestamp: u64,
-    pub hash: String,
     pub message: String
+}
+
+#[derive(Clone)]
+pub struct Transaction {
+    pub tx_info: TxInfo,
+    pub hash: Signature
+}
+
+impl fmt::Display for TxInfo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {} {} {} {} {}", self.payee, self.recipient, self.value, self.gas, self.nonce, self.timestamp, self.message)
+    }
 }
 
 impl Transaction {
@@ -30,16 +44,25 @@ impl Transaction {
     /// # Returns
     /// 
     /// The full Transaction type is returned from this function
-    pub fn new(payee: String, recipient: String, value: u64, gas: u64, nonce: u64, timestamp: u64,  hash: String, message: String) -> Self {
-        Self {
-            payee,
-            recipient,
-            value,
-            gas,
-            nonce,
-            timestamp,
-            hash,
-            message
+    pub fn new(signer: Wallet, recipient: String, value: u64, gas: u64, nonce: u64, timestamp: u64, message: String) -> Result<Self, String> {
+
+        let transaction_info = TxInfo {
+            payee: signer.public_key.clone(),
+            recipient: recipient,
+            value: value,
+            gas: gas,
+            nonce: nonce,
+            timestamp: timestamp,
+            message: message,
+        };
+
+        let info_string = transaction_info.to_string();
+
+        match signer.sign_message(info_string) {
+            Ok(hash) => {
+                Ok(Transaction { tx_info: transaction_info, hash: hash })
+            },
+            Err(e) => return Err(e),
         }
     }
 }
